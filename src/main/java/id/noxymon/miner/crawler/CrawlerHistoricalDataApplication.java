044dff48e384b77e7fe839084cf5f9a24da0c9b3
noxymon <avicienna.u.muqodas@alfacart.com>
2020-12-31 10:47:03 +0700

    Add support for graalvm

--- src/main/java/id/noxymon/miner/crawler/CrawlerHistoricalDataApplication.java
+++ src/main/java/id/noxymon/miner/crawler/CrawlerHistoricalDataApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 public class CrawlerHistoricalDataApplication {
 
     public static void main(String[] args) {
