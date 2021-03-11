7abf0f3eb4b94368c8e84228f51055e8fcf3cc3a
noxymon <avicienna.ulhaq@gmail.com>
2021-03-11 19:43:42 +0700

    Use H2 for integration test

--- src/test/java/id/noxymon/miner/crawler/utils/HmacUtilTest.java
+++ src/test/java/id/noxymon/miner/crawler/utils/HmacUtilTest.java
@@ -8,18 +8,24 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.TestPropertySources;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-@SpringBootTest
-@ExtendWith(MockitoExtension.class)
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 @TestPropertySource(properties = {
-        "application.crawler.enable=false"})
+        "application.crawler.enable=false",
+        "spring.datasource.database=h2",
+        "spring.jpa.hibernate.ddl-auto=create-drop"})
 class HmacUtilTest {
 
     @Test
@@ -36,17 +42,19 @@ class HmacUtilTest {
     @Test
     public void anuan(){
         MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.add("type", "buy");
         map.add("method", "trade");
-        map.add("price", "1000");
         map.add("timestamp", "1606058160000");
-        map.add("idr", "10000");
         map.add("recvWindow", "1606058180000");
-        map.add("pair", "eth_idr");
+        map.add("pair", "xrp_idr");
+        map.add("type", "sell");
+        map.add("xrp", "1.65111111");
+        map.add("price", "7000");
 
-        final UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl("http://example.com")
+        final UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl("https://indodax.com/tapi")
                 .queryParams(map)
                 .build();
-        System.out.println(uriComponents.getQuery());
+        final String hmacResult = HmacUtil.from(uriComponents.getQuery(), "f60617a68fcce028f0a90bc9eb765d17379eb548cc935c01a7ee3186eecf870e9b68f27a31bcfe8d");
+        System.out.println(hmacResult);
+//        System.out.println(uriComponents.getQuery());
     }
 }
